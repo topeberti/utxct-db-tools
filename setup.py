52ea90855949eee5654837231f63a008921ef08f
Alberto Vicente del Egido <alberto.vicente@imdeamaterials.org>
2025-05-06 12:23:00 +0200

    Pip installable dbtools package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dbtools',  # Change this to a valid name, e.g., 'myqueries'
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'psycopg2',
+        'python-dotenv'
+    ],
+    author='Alberto Vicente del Egido',
+    description='Database utilities for IMDEA database',
+    url='https://github.com/topeberti/utxct-db-tools', 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
